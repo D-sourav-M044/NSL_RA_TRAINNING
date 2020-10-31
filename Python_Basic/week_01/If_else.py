36b56072f9a60d7c65aaf956053161f8355e5a41
D.sourav <45461265+D-sourav-M044@users.noreply.github.com>
2020-11-01 01:41:24 +0600

    Create If_else.py
    Basic_python

---
+++ Python_Basic/week_01/If_else.py
@@ -0,0 +1,12 @@
+i =int(input())
+if i<7:
+    print("i is smaller than 7")
+elif i>7:
+    print("i is bigger than 7")
+else:
+    print("i is equal to 7")
+
+for i in range(10):
+    print(i)
+else:
+    print("done")
