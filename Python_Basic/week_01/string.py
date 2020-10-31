84ca28ca4ea194f6b2aa99facd9ba23ca51b13bb
D.sourav <45461265+D-sourav-M044@users.noreply.github.com>
2020-10-31 23:21:42 +0600

    Create string.py
    Basic python

---
+++ Python_Basic/week_01/string.py
@@ -0,0 +1,28 @@
+#string_Formating
+
+msg ="my name is : {x} ".format(x='sourav')
+data = "here is my name : {x} and i want {y} money from you.".format(x='sourav',y=250)
+print(msg)
+print(data)
+
+#string_join:
+
+print(",".join(['apple','mango']))
+print(msg)
+print(msg.replace("sourav","dipto"))
+
+#startswith/endswith
+print("this is ok".startswith("this"))
+print("This is ok".startswith("this"))
+print("This is a statement".endswith("statement"))
+
+#Upper/Lower
+print("hello buddy".upper())
+print("HEy dude".lower())
+
+#split
+txt = "hello, my name is Peter, I am 26 years old"
+
+x = txt.split(", ")
+
+print(x)
