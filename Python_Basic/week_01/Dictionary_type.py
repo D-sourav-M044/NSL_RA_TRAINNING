17605a5a01ec546b6ae384ca5b0b1593bf759caf
D.sourav <45461265+D-sourav-M044@users.noreply.github.com>
2020-11-01 19:50:18 +0600

    dictionary & it's method

---
+++ Python_Basic/week_01/Dictionary_type.py
@@ -0,0 +1,13 @@
+my_marks= {"bangla" : 80,"english":78,"math":[90,87,88]}
+print(my_marks["math"])
+print(my_marks.keys())
+test = { True : 'ok'}
+print(test[True])
+
+my_dic={1:"one",2:"two"}
+print(my_dic)
+my_dic[3]="three"
+print(my_dic)
+
+#get_method
+
