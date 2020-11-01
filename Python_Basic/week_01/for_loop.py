9de5102131dfcf9ca2fbdef06cc76d4d31ef3685
D.sourav <45461265+D-sourav-M044@users.noreply.github.com>
2020-11-01 18:50:48 +0600

    For_loop for python

---
+++ Python_Basic/week_01/for_loop.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    print("bn")
+fruit = ["mango","apple","pineapple","grape"]
+for i in fruit:
+    print(i+ " tree")
+    i=0
+for i in range(10):
+    if i==5:
+        continue
+    elif i==8:
+        break
+    print(i)
+
